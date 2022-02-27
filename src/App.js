6cf129f4a92d124bc4920446cd83588647d03c33
varun-pradhan <varunpradhan331@gmail.com>
2022-02-28 01:31:43 +0530

    Intial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import appStyles from "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [userData, setUserData] = useState([]);
+   
+  const fetchData = async function() {
+    const url = "https://reqres.in/api/users?page=1";
+    const response = await fetch(url);
+    const data = await response.json();
+    setUserData(data.data);
+};
+
+return (
+  <div>
+    <div className="navBar">
+      <h2 className="logo">LetsGrowMore</h2>
+      <button onClick ={fetchData} className="navButton">Get Users</button>
     </div>
-  );
+    <h1 id="containerHeading">User Data</h1>
+    <div className="container">
+      {userData.map(function(user){
+        return(
+          <div className="card">
+            <img src={user.avatar} key = {user.avatar} className="image" alt="logo512.png" />
+            <h3 className="userName" key ={user.first_name}  >{user.first_name} {user.Last_name}</h3>
+            <p className="mailId" key = {user.email} >{user.email}</p>
+          </div>
+        );
+      })}
+    </div>
+  </div>
+);
 }
 
 export default App;
